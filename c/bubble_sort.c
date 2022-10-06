b0288c5d1ab5fc6e895bc4cc421dc790e28d8524
Arghadip Biswas <88325327+arghadipbiswas@users.noreply.github.com>
2022-10-06 19:10:42 +0530

    logic has been change by arghadip

--- c/bubble_sort.c
+++ c/bubble_sort.c
@@ -9,20 +9,18 @@ int main()
 	{
 		scanf("%d",&a[i]);
 	}
-	// for (i=0;i<n-1;i++)
-	// {
-	// 	for (j=0;j<n-1-i;j++)
-	// 	{
-	// 		if (a[j]>a[j+1])
-	// 		{
-	// 		temp=a[j];
-	// 		a[j]=a[j+1];
-	// 		a[j+1]=temp;
-	// 	    }
-	// 	}
-	// }
-
-				// OR
+	for (i=0;i<n;i++)
+	{
+		for (j=i+1;j<n;j++)
+		{
+			if (a[i]>a[j])
+			{
+			temp=a[i];
+			a[i]=a[j];
+			a[j]=temp;
+		    }
+		}
+	}
 
 	
 	printf("\nSorting Elements are: ");
