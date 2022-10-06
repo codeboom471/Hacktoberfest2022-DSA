bd674774a1c8714ac08bf73118795a91e207be89
codeboom471 <115161604+codeboom471@users.noreply.github.com>
2022-10-06 19:09:53 +0530

    Update bubble_sort.c

--- c/bubble_sort.c
+++ c/bubble_sort.c
@@ -24,18 +24,7 @@ int main()
 
 				// OR
 
-	for (i=0;i<n;i++)
-	{
-		for (j=i+1;j<n;j++)
-		{
-			if (a[i]>a[j])
-			{
-			temp=a[i];
-			a[i]=a[j];
-			a[j]=temp;
-		    }
-		}
-	}
+	
 	printf("\nSorting Elements are: ");
 	for (i=0;i<n;i++)
 	{
