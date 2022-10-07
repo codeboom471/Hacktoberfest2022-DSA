d164785c0336c162da9d20da512b8f9d0f3f0c02
Pragati-64 <pragatigupta234@gmail.com>
2022-10-07 22:46:11 +0530

    added some cpp code files

---
+++ cpp/merge_overlapping_intervals.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& interval)
+    {
+     sort(interval.begin(),interval.end()); 
+        vector<vector<int>> ans;
+     auto curr=interval[0];
+        int i=-1;
+        while(++i<interval.size())
+        {
+            if(curr[1]>=interval[i][0])
+                curr[1]=max(curr[1],interval[i][1]);
+            else
+            { ans.push_back(curr);
+               curr=interval[i];}
+        }
+        ans.push_back(curr);
+        return ans;
+    }
+};
