14e39e2419bc2dd3e98b4cf0aece3b8a16708152
Vaskar <vaskarpaul053@gmail.com>
2022-10-10 00:56:09 +0530

    Adding Linkedlist in DSA

---
+++ cpp/linkedlist/insert.cpp
@@ -0,0 +1,37 @@
+#include<stdio.h>  
+#include<stdlib.h>  
+void beginsert(int);  
+struct node  
+{  
+    int data;  
+    struct node *next;  
+};  
+struct node *head;  
+void main ()  
+{  
+    int choice,item;  
+    do   
+    {  
+        printf("\nEnter the item which you want to insert?\n");  
+        scanf("%d",&item);  
+        beginsert(item);  
+        printf("\nPress 0 to insert more ?\n");  
+        scanf("%d",&choice);  
+    }while(choice == 0);  
+}  
+void beginsert(int item)  
+    {  
+        struct node *ptr = (struct node *)malloc(sizeof(struct node *));  
+        if(ptr == NULL)  
+        {  
+            printf("\nOVERFLOW\n");  
+        }  
+        else  
+        {  
+            ptr->data = item;  
+            ptr->next = head;  
+            head = ptr;  
+            printf("\nNode inserted\n");  
+        }  
+          
+    }  
