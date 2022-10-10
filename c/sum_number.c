43f55420de7880e783b8ef69b2381a71d51e03f2
RAJEEVGUPTA950 <115412135+RAJEEVGUPTA950@users.noreply.github.com>
2022-10-10 09:05:30 +0530

    Create sum_number.c

---
+++ c/sum_number.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int sum=0,n,rem;
+	printf("enter the number :");
+	scanf("%d",n);
+while(n>0)
+{
+	rem=n%10;
+	sum=sum+rem;
+	n=n/10;
+}
+printf("%d",sum);
+}
